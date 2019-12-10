27ccdcffcefcf995ea39953d9590507986a5c5d9
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2019-12-10 18:05:54 +0500

    code updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function render(data){
       xValue  = d => d.timestamp,
       yValue = d => d.temprature,
       circleRadius = 5;
+      let tooltips = d3.select("#area").append("div").attr("class","tooltips")
 
 
       let xScale = d3.scaleTime()
@@ -14,7 +15,7 @@ function render(data){
           .range([0 , barWidth]);
       let yScale = d3.scaleLinear()
           .domain(d3.extent(data,yValue))
-          .range([barHeight,0]);
+          .range([barHeight,0]).nice();
       let areaGenrator = d3.area()
               .x(d=>xScale(xValue(d)))
               .y0(barHeight)
@@ -34,11 +35,18 @@ function render(data){
                   .attr("cy",d=>yScale(yValue(d)))
                   .attr("r",circleRadius)
                   .attr("class","circle")
+                  .on("mouseover",function(d,i){
+                     let dt = new Date(d.timestamp)
+                     tooltips.html(`<div><p>Temperature ${Math.round(d.temperature)}<b>F</b> <br><b>Month no:</b><i>${dt.getMonth()}</i></p> </div>`)
+                        .style("top",  ( d3.event.pageY - 114 ) + "px")
+                        .style("left", ( d3.event.pageX - 25 ) + "px")
+                  })
+                  .on("mouseout",function(d,i){
+                    tooltips.html("")
+                  });
       circle.transition()
             .attr("cx",d=>xScale(xValue(d)))
             .duration(1000)
-
-
 
       let xGroup = g.append("g").call(xAxis).attr("transform",`translate(0,${barHeight})`).attr("class","xAxis")
           xGroup.select(".domain").remove()
@@ -46,7 +54,7 @@ function render(data){
           yGroup.select(".domain").remove()
       let time = g.append("text").attr("x",barWidth/2).attr("y",10).text("Time").attr("transform",`translate(0 , ${innerHeight/2 + 370})`).attr("text-anchor","middle").attr("class","text")
       let temprature = g.append("text").attr("x",0).attr("y",0).attr("transform",`rotate(-90) translate(${barHeight/barWidth - 290  },${barWidth/barHeight - 50}) `).text("Temprature").attr("class","text").attr("text-anchor","middle")
-      let title  = g.append("text").attr("x",0).attr("y",0).attr("transform",`translate(${barWidth/2-70} , ${-barHeight/2+351})`).text("Temprature vs Time Graph").attr("class","text")
+      let title  = g.append("text").attr("x",barWidth/2).attr("y",-12).text("Temprature vs Time Graph").attr("class","text")
 }
 
 
